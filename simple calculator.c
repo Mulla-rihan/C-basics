ff0cfcd1796a798724291c3038814a60312fa0b6
Mulla-rihan <Sakolkar3544@gmail.com>
2025-09-10 17:02:50 +0530

    Create simple calculator.c
    Implemented a C program that performs addition, subtraction, multiplication, and division of two numbers

---
+++ simple calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int num1,num2;
+  printf("Enter first number =");
+  scanf("%d",&num1);
+  printf("Enter second number =");
+  scanf("%d",&num2);
+  printf("Add of two numbers :%d\n",num1+num2);
+  printf("Sub of two numbers :%d\n",num1-num2);
+  printf("Mul of two numbers :%d\n",num1*num2);
+  printf("Div of two numbers :%d",num1/num2);
+  return 0;
+}
